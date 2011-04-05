f22ef73c6c67ef3e23c6f46f901b45df6d96fcbf
Katy Jockelson <katyjockelson@thirdsectordesign.org>
2011-04-05 18:13:26 +0100

    removing stray characters from page.tpl.php

--- drupal/sites/all/themes/tsd/page.tpl.php
+++ drupal/sites/all/themes/tsd/page.tpl.php
@@ -27,7 +27,7 @@
 		<div id="content-wrapper">
 			<div class="center_content">
 				<div id="main">
-					<?php if (!empty($title)) : ?> : ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+					<?php if (!empty($title)) : ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
 					<!--tabs visible when logged in-->
 					<?php if (!empty($tabs)): ?><div class="tabs"><?php print $tabs; ?></div><?php endif; ?>
 					<?php if (!empty($messages)): print $messages; endif; ?>

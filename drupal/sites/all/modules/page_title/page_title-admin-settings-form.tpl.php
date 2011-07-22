b71f2b83a18a58ea70f811120ad6689a20b2a5f5
Peter McAndrew <petermcandrew@thirdsectordesign.org>
2011-07-22 14:57:31 +0100

    updated core to 6.22 & updated modules

--- drupal/sites/all/modules/page_title/page_title-admin-settings-form.tpl.php
+++ drupal/sites/all/modules/page_title/page_title-admin-settings-form.tpl.php
@@ -1,9 +1,8 @@
 <?php
-// $Id: page_title-admin-settings-form.tpl.php,v 1.1.2.5 2009/10/31 22:18:54 njt1982 Exp $
 
 /**
  * @file
- * Tempalte file for the admin settings form. Displays configuration in a neat table
+ * Template file for the admin settings form. Displays configuration in a neat table
  */
 
 $rows = array();
@@ -17,15 +16,29 @@ foreach (element_children($form['pattern']) as $key) {
   unset($form['pattern'][$key]['#title']);
 
   $row = array(
-    drupal_render($title),
-    drupal_render($form['scope'][$key]),
-    drupal_render($form['pattern'][$key]),
-    isset($form['showfield'][$key .'_showfield']) ? drupal_render($form['showfield'][$key .'_showfield']) : '',
+    array('data' => drupal_render($title), 'class' => 'page-type'),
+    array('data' => drupal_render($form['scope'][$key]), 'class' => 'scope'),
   );
+  if (isset($form['showfield'][$key .'_showfield'])) {
+    $row[] = array('data' => drupal_render($form['pattern'][$key]), 'class' => 'pattern');
+    $row[] = array('data' => drupal_render($form['showfield'][$key .'_showfield']), 'class' => 'showfield');
+  }
+  else {
+    $row[] = array('data' => drupal_render($form['pattern'][$key]), 'colspan' => 2, 'class' => 'pattern');
+  }
 
   $rows[] = $row;
 }
 
-print theme('table', array(t('Page Type'), t('Token Scope'), t('Pattern'), t('Show Field')), $rows);
+$headers = array(
+  array('data' => t('Page Type'),   'class' => 'page-type'),
+  array('data' => t('Token Scope'), 'class' => 'scope'),
+  array('data' => t('Pattern'),     'class' => 'pattern'),
+  array('data' => t('Show Field'),  'class' => 'showfield'),
+);
+
+
+drupal_add_css(drupal_get_path('module', 'page_title') .'/page_title.admin.css', 'module', 'all', FALSE);
+print theme('table', $headers, $rows, array('id' => 'page-title-settings'));
 
 print drupal_render($form);
